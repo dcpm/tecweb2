f1061702faba17018a92c59cdb0ce7afac4f893a
dcpm <37427885+dcpm@users.noreply.github.com>
2019-10-01 21:51:16 -0300

    Add files via upload

---
+++ prova.php
@@ -0,0 +1,14 @@
+<?php
+
+$nome= $_REQUEST['name'];
+$idade= $_REQUEST['idade'];
+$f= $_REQUEST['filho'];
+
+$formulario = $_GET;
+foreach($formulario as $key=>$value) {
+echo "<p> chave[$key] / valor ==$value </p>";
+
+
+
+
+}
